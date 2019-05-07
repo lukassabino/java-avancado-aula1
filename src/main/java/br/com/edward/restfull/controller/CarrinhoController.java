085c71cf466450ce903e6a2eaaf01e48e150e80f
lukassabinoo <lukassabino_@hotmail.com>
2019-05-07 17:28:12 -0300

    Alteração do carrinho

--- src/main/java/br/com/edward/restfull/controller/CarrinhoController.java
+++ src/main/java/br/com/edward/restfull/controller/CarrinhoController.java
@@ -3,15 +3,41 @@ package br.com.edward.restfull.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import br.com.edward.restfull.controller.ProdutoController;
+import br.com.edward.restfull.model.ItemCarrinhoModel;
 
 @RestController
 @RequestMapping("/carrinho")
 public class CarrinhoController {
 
-	public static final List<ProdutoController> listaCarrinho = new ArrayList<ProdutoController>();
+	public static final List<ItemCarrinhoModel> listaCarrinho = new ArrayList<ItemCarrinhoModel>();
+
+	@PostMapping("/post")
+	public ItemCarrinhoModel post(@RequestBody ItemCarrinhoModel id) {
+		listaCarrinho.add(id);
+		return id;
+	}
+
+	@GetMapping("/mostrar")
+	public List<ItemCarrinhoModel> mostrarTudo() {
+		return listaCarrinho;
+	}
+
+	@GetMapping("/remover")
+	public ItemCarrinhoModel remover(@RequestParam Long id) {
+		ItemCarrinhoModel produtoRemover = null;
+		for (ItemCarrinhoModel produto : listaCarrinho) {
+			if (id.equals(produto.getId())) {
+				produtoRemover = produto;
+				break;
+			}
+		}
+		return produtoRemover;
+	}
 
 }
