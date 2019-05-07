b6cd55c566bb87c3a08000412088d5cd99684cba
lukassabinoo <lukassabino_@hotmail.com>
2019-05-07 13:23:11 -0300

    Exercicio

--- src/main/java/br/com/edward/restfull/controller/PessoaController.java
+++ src/main/java/br/com/edward/restfull/controller/PessoaController.java
@@ -1,5 +1,11 @@
 package br.com.edward.restfull.controller;
 
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +19,9 @@ import br.com.edward.restfull.model.PessoaModel;
 @RestController
 @RequestMapping("/pessoa")
 public class PessoaController {
-
+	
+	private static List<PessoaModel> lista = new ArrayList<PessoaModel>();
+	
     @GetMapping("/param")
     public PessoaModel param(@RequestParam String nome) {
         return new PessoaModel(nome);
@@ -26,6 +34,28 @@ public class PessoaController {
 
     @PostMapping("/post")
     public PessoaModel post(@RequestBody PessoaModel model) {
+    	lista.add(model);
         return model;
     }
+   
+    @GetMapping("/mostrar-tudo")
+    public List<PessoaModel> mostrarTudo() {
+    	return lista;
+    }
+    
+    @GetMapping("/remover")
+    public PessoaModel remover(@RequestParam Long id) {
+    	PessoaModel pessoaRemover = null;
+    	for (PessoaModel item : lista) {
+    		if (id.equals(item.getId())) {
+    			pessoaRemover = item;
+    			break;
+    		}
+    	}
+		if  (Objects.nonNull(pessoaRemover)) {
+			lista.remove(pessoaRemover);
+		}
+		return pessoaRemover;
+    	
+  }
 }
--- src/main/java/br/com/edward/restfull/model/PessoaModel.java
+++ src/main/java/br/com/edward/restfull/model/PessoaModel.java
@@ -3,9 +3,30 @@ package br.com.edward.restfull.model;
 import java.time.ZonedDateTime;
 
 public class PessoaModel {
+	
 
 	private ZonedDateTime dataCriacao;
 	private String nome;
+	private String sobrenome;
+	private int idade;
+	private double peso;
+	private Long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
 
 	public PessoaModel() {
 		this.dataCriacao = ZonedDateTime.now();
